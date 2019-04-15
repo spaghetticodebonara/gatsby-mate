39646f0c3485f0da4132f12d6aa8a6917b90cfd2
spaghetticodebonara <spaghetticodebonara@gmail.com>
2019-04-15 02:30:07 +0200

    first commit

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#b4b4b4',
+
+  primary: '#333',
+  primaryLight: '#333',
+  primaryDark: '#333',
+
+  secondary: '#f8d96c',
+  secondaryLight: '#f8d96c',
+  secondaryDark: '#333',
+};
